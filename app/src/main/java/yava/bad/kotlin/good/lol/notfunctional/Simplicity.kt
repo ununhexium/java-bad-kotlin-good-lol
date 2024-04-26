01e2a42e5c80c155023686990b28bb6c17c02ea4
Christophe '116' Loiseau <116@lab0.net>
2024-04-26 10:19:16 +0200

    Quick clarity update

--- app/src/main/java/yava/bad/kotlin/good/lol/notfunctional/Simplicity.kt
+++ app/src/main/java/yava/bad/kotlin/good/lol/notfunctional/Simplicity.kt
@@ -5,7 +5,7 @@ typealias in1 = (o1: Any) -> Any
 typealias in0 = () -> Any
 
 interface Forced<T> {
-  operator fun invoke(t: T): T // analog to (T) -> T
+    operator fun invoke(t: T): T // analog to (T) -> T
 }
 
 fun interface Lib1Kt<T> : (T) -> T
@@ -13,69 +13,102 @@ fun interface Lib1Kt<T> : (T) -> T
 fun interface Lib2Kt<T> : (T) -> T
 
 fun interface Lib3Kt<T> {
-  fun weirdName(t: T): T
+    fun weirdName(t: T): T
 }
 
 fun main() {
-  /*
-   * 1 notation to rule them all
-   * (InputType1, InputType2, InputType3, ...) -> OutputType
-   */
+    /*
+     * 1 notation to rule them all
+     * (InputType1, InputType2, InputType3, ...) -> OutputType
+     */
 
-  val biConsumer: in2 = { o1, o2 -> }
-  val biFunction: in2 = { o1, o2 -> Any() }
-  val binaryOperator: in2 = { o1, o2 -> Any() }
-  val biPredicate: in2 = { o1, o2 -> true }
-  val booleanSupplier: () -> Boolean = { true }
-  val consumer: in1 = {}
-  val function: in1 = { it }
-  val predicate: in1 = { true }
-  val supplier: in0 = { Any() }
-  val unaryOperator: in1 = { it -> Any() }
-  val runnable: in0 = {/* do stuff here */ }
+    val biConsumer: (Any, Any) -> Unit = { o1, o2 -> }
+    val biFunction: in2 = { o1, o2 -> Any() }
+    val binaryOperator: in2 = { o1, o2 -> Any() }
+    val biPredicate: (Any, Any) -> Boolean = { o1, o2 -> true }
+    val booleanSupplier: () -> Boolean = { true }
+    val consumer: (Any) -> Unit = {}
+    val function: (Any) -> Any = { it }
+    val predicate: (Any) -> Boolean = { true }
+    val supplier: () -> Any = { Any() }
+    val unaryOperator: (Any) -> Any = { it -> Any() }
+    val runnable: () -> Unit = {/* do stuff here */ }
 
-  val threeInputs: (Any, Any, Any) -> Any = { a, b, c -> Any() }
-  val threeInputs2 = { a: Any, b: Any, c: Any -> Any() }
+    someMoreRealLifeExamples()
 
-  val operation1: Lib1Kt<Int> = Lib1Kt { it + 1 }
-  val operation = { it: Int -> it + 1 }
-  // works!
-  callAny(operation1)
-  callLib1(operation1)
-  callLib2(operation1)
+    val threeInputs: (Any, Any, Any) -> Any = { a, b, c -> Any() }
+    val threeInputs2 = { a: Any, b: Any, c: Any -> Any() }
 
-  callAny(operation)
-  callLib1(operation)
-  callLib2(operation)
+    val operation1: Lib1Kt<Int> = Lib1Kt { it + 1 }
+    val operation = { it: Int -> it + 1 }
+    // works!
+    callAny(operation1)
+    callLib1(operation1)
+    callLib2(operation1)
+
+    callAny(operation)
+    callLib1(operation)
+    callLib2(operation)
 
 
-  // can force a specific interface
-  val force = object : Forced<Int> {
-    override fun invoke(t: Int): Int = t + 1
-  }
-  forced(force)
-  // not accepted
-  // forced(operation1)
+    // can force a specific interface
+    val force = object : Forced<Int> {
+        override fun invoke(t: Int): Int = t + 1
+    }
+    forced(force)
+    // not accepted
+    // forced(operation1)
 
-  // doesn't work in the interface doesn't comply to a functional interface
-  val weirdOperation = Lib3Kt<Int> { it + 1 }
-  //  callAny(weirdOperation)
-  //  callLib1(weirdOperation)
-  //  callLib2(weirdOperation)
+    // doesn't work in the interface doesn't comply to a functional interface
+    val weirdOperation = Lib3Kt<Int> { it + 1 }
+    //  callAny(weirdOperation)
+    //  callLib1(weirdOperation)
+    //  callLib2(weirdOperation)
+}
+
+fun someMoreRealLifeExamples() {
+    val list1 = 0..10
+    // java: Function
+    val selector: (Int) -> Boolean = { it % 2 == 0 }
+    println(list1.groupBy(selector))
+    // or just
+    println(list1.groupBy { it % 2 == 0 })
+
+    val list2 = 0..10L
+    // java: LongToDoubleFunction
+    val longToDouble: (Long) -> Double = { it.toDouble() }
+    println(list2.map(longToDouble))
+    // or just
+    println(list2.map { it.toDouble() })
+
+    class A
+    class B
+    class C
+
+    val shoppingList = listOf(
+            // Item to Amount
+            A() to 2,
+            B() to 5,
+            C() to 3
+    )
+    val prices = mapOf(A::class to 7.99, B::class to 1.99, C::class to 129.99)
+    val sumByItem: (Pair<Any, Int>) -> Double = { (obj, amount) -> prices[obj::class]!! * amount }
+    val total = shoppingList.map(sumByItem).sum()
+    println(total)
 }
 
 fun callAny(f: (Int) -> Int) =
-  f(1)
+        f(1)
 
 fun callLib1(f: Lib1Kt<Int>) =
-  f(1)
+        f(1)
 
 fun callLib2(f: Lib2Kt<Int>) =
-  f(1)
+        f(1)
 
 fun forced(f: Forced<Int>) {
-  // can still be called like a function
-  f(1)
-  // or like an interface
-  f.invoke(1)
+    // can still be called like a function
+    f(1)
+    // or like an interface
+    f.invoke(1)
 }
